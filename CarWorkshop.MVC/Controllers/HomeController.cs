275819bec2045a0919bf0df918a27f2bb7e49d86
kamilmaly <k.malysa94@gmail.com>
2024-03-09 21:50:36 +0100

    Add project files.

---
+++ CarWorkshop.MVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CarWorkshop.MVC.Models;
+
+namespace CarWorkshop.MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        var model = new List<Person>()
+        {
+            new Person()
+            {
+                FirstName = "Tom",
+                LastName = "Lik"
+            },
+            new Person()
+            {
+                FirstName = "Kris",
+                LastName = "Wik"
+            }
+        };
+        return View(model);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
