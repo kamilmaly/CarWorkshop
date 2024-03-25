3dda6b31b1910cb1e03dae1d3175e8723265da1c
kamilmaly <k.malysa94@gmail.com>
2024-03-25 12:23:59 +0100

    Data seeding

---
+++ CarWorkshop.Infrastructure/Seeders/CarWorkshopSeeder.cs
@@ -0,0 +1,45 @@
+﻿using CarWorkshop.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Infrastructure.Seeders
+{
+    public class CarWorkshopSeeder
+    {
+        private readonly CarWorkshopDbContext _dbContext;
+
+        public CarWorkshopSeeder(CarWorkshopDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Seed()
+        {
+            if (await _dbContext.Database.CanConnectAsync())
+            {
+                if (!_dbContext.CarWorkshops.Any())
+                {
+                    var mazdaAso = new Domain.Entities.CarWorkshop()
+                    {
+                        Name = "Mazda ASO",
+                        Description = "Service",
+                        ContactDetails = new()
+                        {
+                            City = "Munchen",
+                            Street = "Bee",
+                            PostalCode = "88-023",
+                            PhoneNumber = "+498888888888"
+                        }
+                    };
+                    mazdaAso.EncodeName();
+
+                    _dbContext.CarWorkshops.Add(mazdaAso);
+                    await _dbContext.SaveChangesAsync();
+                }
+            }
+        }
+    }
+}
