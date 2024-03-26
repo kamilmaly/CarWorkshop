dd40580e40b302dd6db1bd2c0f5f3154616235bd
kamilmaly <k.malysa94@gmail.com>
2024-03-26 11:54:30 +0100

    Fluent Validation

--- CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
+++ CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
@@ -9,15 +9,15 @@ namespace CarWorkshop.Application.CarWorkshop
 {
     public class CarWorkshopDto
     {
-        [Required]
-        [StringLength(20,MinimumLength = 2)]
+        //[Required]
+        //[StringLength(20,MinimumLength = 2)]
         public string Name { get; set; } = default!;
         
-        [Required(ErrorMessage = "Please insert value")]
+        //[Required(ErrorMessage = "Please insert value")]
         public string? Description { get; set; }
         public string? About { get; set; }
 
-        [StringLength(12, MinimumLength = 8)]
+        //[StringLength(12, MinimumLength = 8)]
         public string? PhoneNumber { get; set; }
         public string? Street { get; set; }
         public string? City { get; set; }
--- CarWorkshop.Application/Extensions/ServiceCollectionExtensions.cs
+++ CarWorkshop.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
-﻿using CarWorkshop.Application.Mappings;
+﻿using CarWorkshop.Application.CarWorkshop;
+using CarWorkshop.Application.Mappings;
 using CarWorkshop.Application.Services;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -16,6 +19,10 @@ namespace CarWorkshop.Application.Extensions
             services.AddScoped<ICarWorkshopService, CarWorkshopService>();
 
             services.AddAutoMapper(typeof(CarWorkshopMappingProfile));
+
+            services.AddValidatorsFromAssemblyContaining<CarWorkshopDtoValidator>()
+                .AddFluentValidationAutoValidation()
+                .AddFluentValidationClientsideAdapters();
         }
     }
 }
