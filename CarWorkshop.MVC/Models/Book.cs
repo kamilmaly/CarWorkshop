b76981ff9dd2a96b8913ed349c72f1c32faf93a2
kamilmaly <k.malysa94@gmail.com>
2024-03-29 15:26:15 +0100

    Adding notification - toastr package

--- CarWorkshop.MVC/Models/Book.cs
+++ CarWorkshop.MVC/Models/Book.cs
@@ -5,6 +5,6 @@
         public string? Title { get; set; }
         public string? Description { get; set; }
 
-        public List<string> Tags { get; set; } 
+        public List<string> Tags { get; set; }
     }
 }
